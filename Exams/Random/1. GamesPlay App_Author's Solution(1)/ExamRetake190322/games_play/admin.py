b025ae1d91d9976b0fb723c56383519773233f28
milenalilova <milenalilova78@gmail.com>
2024-09-02 17:54:55 -0300

    Removed old projects

--- Exams/Random/1. GamesPlay App_Author's Solution(1)/ExamRetake190322/games_play/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from games_play.models import ProfileModel, GameModel
-
-admin.site.register(ProfileModel)
-admin.site.register(GameModel)
--- Exams/Random/1. GamesPlay App_Author's Solution(1)/ExamRetake190322/games_play/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GamesPlayConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'games_play'
